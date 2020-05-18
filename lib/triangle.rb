b175a2e0743487c8b2fbb0f52c6ebd716afd7b53
Anna Lux <anna.watt@flatironschool.com>
2020-05-18 12:00:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,20 +10,22 @@ class Triangle
   end
 
   def kind
-    # triangle = Triangle.new
-    # if triangle.side_one <= 0 || triangle.side_two <= 0 || triangle.side_three <= 0
-    #   raise TriangleError
-    sides = [@side_one, @side_two, @side_three]
-    if sides[0] <= 0 || sides[1] <= 0 || sides[2] <= 0
-      raise TriangleError
-    elsif sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]
-      raise TriangleError
-    elsif @side_one == @side_two && @side_one == @side_three
+    validate_triangle
+    if @side_one == @side_two && @side_one == @side_three
       :equilateral
     elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
       :isosceles
     else
       :scalene
+    end
+  end
+
+  def validate_triangle
+    sides = [@side_one, @side_two, @side_three]
+    if sides[0] <= 0 || sides[1] <= 0 || sides[2] <= 0
+      raise TriangleError
+    elsif sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]
+      raise TriangleError
     end
   end
 
